760f22d837669dac2d9c80d5f6d5003e7d4e33cc
Tangi <tangui.favennec@gmail.com>
2020-03-12 11:25:55 +0900

    remote resource validation

--- app/model/resizing_job.js
+++ app/model/resizing_job.js
@@ -2,6 +2,7 @@
 
 var fs                 = require('fs');
 var crypto             = require('crypto');
+var request            = require('request');
 const config           = require('../../config');
 
 class ResizingJob {
@@ -26,5 +27,31 @@ ResizingJob.prototype.isAlreadyCached = function (filename, cb) {
   });
 };
 
+ResizingJob.prototype.validateRemoteSource = function (cb) {
+  if (!url.parse(this.options.url).hostname) {
+    return cb(`Invalid hostname for resource ${this.options.url}`, 400);
+  }
+
+  var options = {
+    url: this.options.url,
+    timeout: config.imageRetrievalTimeOut
+  };
+
+  request.head(options, function (err, res, body) {
+    if (err) {
+      if (err.code === 'ETIMEDOUT') {
+        cb('Image retrieval itmed out, ETIMEDOUT');
+      } else {
+        cb('Image server side', 500);
+      }
+    } else if (res.statusCode !== 200) {
+      cb('Unexpected response' ,res.statusCode);
+    } else if (res.headers['content-type'].split('/')[0] !== 'image') {
+      cb('Unexpected content type', 415);
+    } else {
+      cb('Image successfully retrieved', 200);
+    }
+  });
+};
 
 module.exports = ResizingJob
--- config.js
+++ config.js
@@ -4,7 +4,8 @@ const config = {
   appPort: process.env.PORT || 3000,
   appStdOut: true,
   cacheDirectory: __dirname + '/cache/',
-  cacheHash: 'sha1'
+  cacheHash: 'sha1',
+  imageRetrievalTimeOut: 5000
 };
 
 module.exports = config;
